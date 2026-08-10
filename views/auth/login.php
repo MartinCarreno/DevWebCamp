d20abc15904ab710cdd01ef570565e2153f224a2
Martin Carreno <martincarreno71@gmail.com>
2026-08-10 15:17:20 -0400

    First Commit
    Carga inicial del proyecto, hasta el momento solo pag de login con una
    pequena navegacion

---
+++ views/auth/login.php
@@ -0,0 +1,36 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Inicia Sesion en DevWebCamp</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input
+                type="email"
+                class="formulario__input"
+                placeholder="Tu Email"
+                id="email"
+                name="email"
+            >
+        </div>
+
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password</label>
+            <input
+                type="password"
+                class="formulario__input"
+                placeholder="Tu Password"
+                id="password"
+                name="password"
+            >
+        </div>
+
+        <input type="submit" class ="formulario__submit" value="Iniciar Sesion">
+
+        <div class="acciones">
+            <a href="/registro" class="acciones__enlace">Crear Cuenta</a>
+            <a href="/olvide" class="acciones__enlace">Recuperar Password</a>
+        </div>
+
+    </form>
+</main>
