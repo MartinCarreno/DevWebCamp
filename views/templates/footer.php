fa62799e2ffc14d302f5b1acde880f4403a2047b
Martin Carreño <martincarreno71@gmail.com>
2026-08-11 10:51:21 -0400

    feat(footer): estructura muy pero muy basica
    En proximos commits estaran los estilos y mas estructura en PHP

---
+++ views/templates/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="header__logo">
+                &#60;DevWebCamp />
+            </h3>
+        </div>
+    </div>
+</footer>
