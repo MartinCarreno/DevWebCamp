fb2410313059d3f59c9c87147a8802b963afd50a
Martin Carreño <martincarreno71@gmail.com>
2026-08-25 09:31:46 -0400

    feat(ponentes): registradas nuevas clases para dar estilos proximamente

--- views/admin/ponentes/formulario.php
+++ views/admin/ponentes/formulario.php
@@ -1,5 +1,5 @@
-<fieldset>
-    <legend>Informacion Personal</legend>
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Informacion Personal</legend>
 
     <div class="formulario__campo">
         <label for="nombre" class="formulario__label">Nombre</label>
