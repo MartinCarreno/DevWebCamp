88b8a889136dfeec788f59ff6e61a1c26fff95db
Martin Carreno <martincarreno71@gmail.com>
2026-09-06 04:53:34 -0300

    feat(tags): ahora se permite mostrar los tags que se registran y tambien eliminarlos con doble click

--- src/js/tags.js
+++ src/js/tags.js
@@ -3,6 +3,9 @@
 
     if(tagsInput){
 
+        const tagsDiv = document.querySelector('#tags');
+        const tagsInputHidden = document.querySelector('[name="tags"]');
+
         let tags = [];
 
         // escuchar los cambios en el input
@@ -21,8 +24,32 @@
 
                 //limpiar input
                 tagsInput.value = '';
-                console.log(tags);
+                
+                mostrarTags();
             }
+        }
+
+        function mostrarTags() {
+            tagsDiv.textContent = '';
+            tags.forEach(tag => {
+                const etiqueta = document.createElement('LI');
+                etiqueta.classList.add('formulario__tag');
+                etiqueta.textContent = tag;
+                etiqueta.ondblclick = eliminarTag;
+                tagsDiv.appendChild(etiqueta);
+            })
+            
+            actualizarInputHidden();
+        }
+
+        function eliminarTag(e) {
+            e.target.remove();
+            tags =tags.filter(tag => tag  !== e.target.textContent)
+            actualizarInputHidden();
+        }
+
+        function actualizarInputHidden() {
+            tagsInputHidden.value = tags.toString();
         }
     }
 })()
