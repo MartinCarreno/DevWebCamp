7ea5e63f9f8714ab4a7ae26d14d13bdc938863a6
Martin Carreno <martincarreno71@gmail.com>
2026-09-06 06:47:01 -0300

    feat(docs): agregada tabla ponentes al script sql

--- docs/sql/schema/01_init.sql
+++ docs/sql/schema/01_init.sql
@@ -24,3 +24,16 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--tabla ponenetes
+CREATE TABLE `ponentes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) DEFAULT NULL,
+  `apellido` varchar(40) DEFAULT NULL,
+  `ciudad` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pais` varchar(20) DEFAULT NULL,
+  `imagen` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `tags` varchar(120) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `redes` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
