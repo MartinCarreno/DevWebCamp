d05cdcb4b94d4363d4da93d67e06f15bc4e190fd
Martin Carreno <martincarreno71@gmail.com>
2026-09-07 02:44:01 -0300

    feat(table): estilos para la tabla de los ponentes

--- views/admin/ponentes/index.php
+++ views/admin/ponentes/index.php
@@ -30,13 +30,13 @@
                         </td>
 
                         <td class="table__td--acciones">
-                            <a href="/admin/ponentes/editar?id=<?php echo $ponente->id;?>">
+                            <a class="table__accion table__accion--editar" href="/admin/ponentes/editar?id=<?php echo $ponente->id;?>">
                                 <i class="fa-solid fa-user-pen"></i>
                                 Editar
                             </a>
 
                             <form class="table__formulario">
-                                <button type="submit">
+                                <button class="table__accion table__accion--eliminar" type="submit">
                                     <i class="fa-solid fa-circle-x-mark"></i>
                                     Eliminar
                                 </button>
