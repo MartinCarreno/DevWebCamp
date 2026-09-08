55c897fc33982e83bbc0394744f469acd2baf8a5
Martin Carreno <martincarreno71@gmail.com>
2026-09-08 04:56:53 -0300

    feat(tags): ahora los tags se recuperan cuando uno esta en el modo editar, o cuando no pasa la validacion ahora los tags se mantienen

--- src/js/tags.js
+++ src/js/tags.js
@@ -8,6 +8,12 @@
 
         let tags = [];
 
+        //recuperar input oculto
+        if(tagsInputHidden.value != '') {
+            tags = tagsInputHidden.value.split(',');
+            mostrarTags();
+        }
+
         // escuchar los cambios en el input
         tagsInput.addEventListener('keypress', guardarTag)
 
