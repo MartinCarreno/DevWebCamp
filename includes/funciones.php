e1082e92bb5d039794cb8ef3fa8ad8c2bb94797b
Martin Carreno <martincarreno71@gmail.com>
2026-09-22 21:41:55 -0300

    feat(funciones): se agregaron funciones de validacion para prohibir el ingreso sin autenticacion

--- includes/funciones.php
+++ includes/funciones.php
@@ -11,6 +11,16 @@ function s($html) : string {
     return $s;
 }
 
-function pagina_actual($path) :bool {
+function pagina_actual($path) : bool {
     return str_contains( $_SERVER['PATH_INFO'], $path) ? true : false;
+}
+
+function is_auth() : bool {
+    session_start();
+    return isset($_SESSION['nombre']) && !empty($_SESSION);
+}
+
+function is_admin() : bool {
+    session_start();
+    return isset($_SESSION['admin']) && !empty($_SESSION);
 }
